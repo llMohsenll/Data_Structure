f13983a89744e8604d004520ec53d1bb9614a7cf
llMohsenll <150347905+llMohsenll@users.noreply.github.com>
2024-05-08 17:59:12 +0330

    Update Polonomial.h

--- Polonomial.h
+++ Polonomial.h
@@ -1,16 +1,1 @@
-#ifndef POLONOMIAL_H
-#define POLONOMIAL_H
 
-
-class Polonomial
-{
-    public:
-        Polonomial() {}
-        virtual ~Polonomial() {}
-
-    protected:
-
-    private:
-};
-
-#endif // POLONOMIAL_H
